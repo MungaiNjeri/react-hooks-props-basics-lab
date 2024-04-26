d6aa63279994aaa8dc2c1255abdbb0eb760492bd
njeri <njeri255mungai@gmail.com>
2024-04-26 10:35:34 +0300

    react hook

--- src/__tests__/About.test.js
+++ src/__tests__/About.test.js
@@ -4,6 +4,8 @@ import { render, screen } from "@testing-library/react";
 import user from "../data/user";
 import About from "../components/About";
 
+
+
 test("renders a <p> element with the bio from props", () => {
   render(<About bio="I made this" links={user.links} />);
   expect(screen.queryByText("I made this")).toBeInTheDocument();
